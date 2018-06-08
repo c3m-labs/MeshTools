492e38680e59aecefb79e7b98ddbe1f0797a182c
PintarM <matevz.pintar@gmail.com>
2018-06-08 11:35:00 +0200

    Bump version and cosmetic updates

--- PacletInfo.m
+++ PacletInfo.m
@@ -3,11 +3,10 @@
 (* Paclet Info File *)
 Paclet[
 	Name -> "MeshTools",
-	Version -> "0.1.0",
+	Version -> "0.2.0",
 	WolframVersion -> "11.+",
     Description -> "Utilities for creating and manipulating ElementMesh objects.",
-    Creator -> "info@c3m.si",
-    Publisher->"C3M d.o.o.",
+    Creator -> "C3M d.o.o. (info@c3m.si)",
     URL -> "https://github.com/c3m-labs/MeshTools",
     Tags -> {"finite-elements","mesh"},
     Categories -> {"FEM"},
