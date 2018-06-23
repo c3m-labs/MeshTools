d424ea13dbe48271bd2c468dd51ba173c164c587
PintarM <matevz.pintar@gmail.com>
2018-06-23 19:44:19 +0200

    Update PacletInfo.m with new resource content

--- PacletInfo.m
+++ PacletInfo.m
@@ -8,10 +8,20 @@ Paclet[
     Description -> "Utilities for creating and manipulating ElementMesh objects.",
     Creator -> "C3M d.o.o. (info@c3m.si)",
     URL -> "https://github.com/c3m-labs/MeshTools",
-    Tags -> {"finite-elements","mesh"},
-    Categories -> {"FEM"},
 	Extensions -> {
-		{"Kernel", Root -> ".", Context ->{"MeshTools`"}},
-		{"Documentation",  Language -> "English", MainPage -> "Guides/MeshTools"}
+		{"Kernel",
+			Root -> ".",
+			Context ->{"MeshTools`"}
+		},
+		{"Documentation",
+			Language -> "English",
+			MainPage -> "Guides/MeshTools"
+		},
+		{"PacletServer",
+			"Tags" -> {"finite-elements","mesh","FEM"},
+			"Categories" -> {"FEM"},
+			"Description" -> "A package with utilities for creating and manipulating ElementMesh objects.",
+			"License" -> "MIT"
+		}
 	}
 ]
