023c8fb98ca103808d6e66a1d04e305690a8f3e0
PintarM <matevz.pintar@gmail.com>
2018-12-15 18:04:44 +0100

    Update paclet build procedure with a script

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,14 +1,16 @@
 (* ::Package:: *)
 
 (* Paclet Info File *)
+(* BuildNumber and Internal values should be inserted during build procedure. *)
 Paclet[
 	Name -> "MeshTools",
 	Version -> "0.3.1",
 	WolframVersion -> "11.+",
 	Description -> "Utilities for creating and manipulating ElementMesh objects.",
-	Creator -> "C3M d.o.o.",
+	Creator -> "Matevz Pintar",
+	Publisher -> "C3M d.o.o.",
 	URL -> "https://github.com/c3m-labs/MeshTools",
-	Thumbnail->"Icon.png",
+	Thumbnail->"FrontEnd/Icon.png",
 	Extensions -> {
 		{"Kernel",
 			Root -> ".",
@@ -18,6 +20,7 @@ Paclet[
 			Language -> "English",
 			MainPage -> "Guides/MeshTools"
 		},
+		(* Metadata for PacletServer (https://paclets.github.io/PacletServer) *)
 		{"PacletServer",
 			"Tags" -> {"finite-elements","mesh","FEM"},
 			"Categories" -> {"FEM"},
