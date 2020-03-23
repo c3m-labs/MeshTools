72e793c9226a15fa26880eb376879efa34addab2
PintarM <matevz.pintar@gmail.com>
2020-03-23 08:32:52 +0100

    Convert PacletInfo keys from symbols to strings

--- MeshTools/PacletInfo.m
+++ MeshTools/PacletInfo.m
@@ -1,16 +1,15 @@
-(* ::Package:: *)
-
-(* Paclet Info File *)
 Paclet[
-	Name -> "MeshTools",
-	Version -> "1.0.1",
-	WolframVersion -> "11.+",
-	Description -> "Utilities for creating and manipulating ElementMesh objects.",
-	Creator -> "Matevz Pintar",
-	Publisher -> "C3M d.o.o.",
-	URL -> "https://github.com/c3m-labs/MeshTools",
-	Thumbnail->"FrontEnd/Icon.png",
-	Extensions -> {
+	"Name" -> "MeshTools",
+	"Version" -> "1.0.1",
+	"WolframVersion" -> "11.+",
+	"Description" -> "Utilities for creating and manipulating ElementMesh objects.",
+	"Creator" -> "Matevz Pintar",
+	"Publisher" -> "C3M d.o.o.",
+	"Category" -> "FEM",
+	"Keywords" -> {"finite-elements","mesh","FEM"},
+	"URL" -> "https://github.com/c3m-labs/MeshTools",
+	"Thumbnail"->"FrontEnd/Icon.png",
+	"Extensions" -> {
 		{"Kernel",
 			Root -> "Kernel",
 			Context ->{"MeshTools`"}
